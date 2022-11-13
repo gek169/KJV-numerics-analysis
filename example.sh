205671e5fe0c20f8d2ae51c49220fec6c451d053
NAN <NAN>
2022-11-12 19:38:48 -0500

    F

--- example.sh
+++ example.sh
@@ -1,5 +1,8 @@
+#run this on a unix system.
+
 make
 
 ./blcount -1 Genesis 1 1 > first_last.out
 ./blcount -1 John 3 16 > twentyseven.out
 ./blcount -1 Revelation 13 18 > rev1318.out
+./blcount -1 Exodus 22 28 > gods.out
